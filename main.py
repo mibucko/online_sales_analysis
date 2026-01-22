78f7145eec3b58699a5d2ec6b26938d4abfc00e9
mibucko <mbucko@tmf.bg.ac.rs>
2026-01-22 14:01:10 +0100

    Created cart.py, updated files main.py and product_manager.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
+import random
 
-p1 = Product("Audi", 1200, 15)
-p2 = Product("Lamborgini", 20, 50)
-p3 = Product("Fica", 250, 5)
-p4 = Product("Lancia", 2150, 45)
-p5 = Product("Skoda", 3, 3000)
+p1 = Product("Audi", 100, 10)
+p2 = Product("Lamborgini", 200, 20)
+p3 = Product("Fica", 300, 30)
+p4 = Product("Lancia", 400, 40)
+p5 = Product("Skoda", 500, 50)
+products = (p1, p2, p3, p4, p5)
 
 pm = ProductManager()
-pm.add_product(p1)
-pm.add_product(p2)
-pm.add_product(p3)
-pm.add_product(p4)
-pm.add_product(p5)
+c = Cart()
+
+for p in products:      # Adding all products to the list
+    pm.add_product(p)
+
+random_items = random.sample(pm.products, 3)
+for i in random_items:
+    c.add_to_cart(i)    # Adding some items to the shopping list
 
 p3.set_quantity(44)     # Testing of function for changing product quantity.
-pm.display_all_products()
+pm.display_info()
 print()
 pm.total_value()        # Total inventory value
-pm.item_remove("Fica")      # Testing of function for removing product.
+pm.item_remove("Fica")  # Testing of function for removing product.
 print()
-pm.display_all_products() # Product list without removed item.
+pm.display_info()       # Product list with removed item.
+print()
+c.display_info()        # List of purchased items and money spent
